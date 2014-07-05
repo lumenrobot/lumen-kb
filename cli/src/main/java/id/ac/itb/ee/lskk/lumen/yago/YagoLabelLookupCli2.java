7aa04586877b32fe488c0f8022b58e75910aec74
Hendy Irawan <ceefour666@gmail.com>
2014-07-05 13:32:47 -0430

    real version

--- cli/src/main/java/id/ac/itb/ee/lskk/lumen/yago/YagoLabelLookupCli2.java
+++ cli/src/main/java/id/ac/itb/ee/lskk/lumen/yago/YagoLabelLookupCli2.java
@@ -3,7 +3,9 @@ package id.ac.itb.ee.lskk.lumen.yago;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.gridgain.grid.Grid;
 import org.gridgain.grid.GridException;
@@ -13,35 +15,33 @@ import org.gridgain.grid.lang.GridReducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class YagoLabelLookupCli {
+public class YagoLabelLookupCli2 {
 
 	private static final Logger log = LoggerFactory
-			.getLogger(YagoLabelLookupCli.class);
-
+			.getLogger(YagoLabelLookupCli2.class);
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException, GridException {
 		String upLabel = "Muhammad";
 		
-		try (Grid grid = GridGain.start(YagoLabelLookupCli.class.getResource("yago.gridgain.xml"))) {
+		try (Grid grid = GridGain.start(YagoLabelLookupCli2.class.getResource("yago.gridgain.xml"))) {
 			GridCache<String, String> labelCache = grid.cache("yagoLabel");
 //			labelCache.queries().createScanQuery(null).execute((GridClosure<Entry<String, String>, String>) (it) ->
 //				it.getValue().equalsIgnoreCase(upLabel) ? it.getKey() : null);
 			log.info("Finding resource for label '{}'...", upLabel);
-			Collection<String> founds = labelCache.queries().createScanQuery(null).execute(new GridReducer<Entry<String, String>, String>() {
-				String id;
+			Collection<Set<String>> founds = labelCache.queries().createScanQuery(null).execute(new GridReducer<Entry<String, String>, Set<String>>() {
+				Set<String> ids = new HashSet<>();
 				
 				@Override
 				public boolean collect(Entry<String, String> e) {
 					if (e.getValue().equalsIgnoreCase(upLabel)) {
-						id = e.getKey();
-						return false;
-					} else {
-						return true;
+						ids.add( e.getKey() );
 					}
+					return true;
 				}
 				
 				@Override
-				public String reduce() {
-					return id;
+				public Set<String> reduce() {
+					return ids;
 				}
 			}).get();
 			
